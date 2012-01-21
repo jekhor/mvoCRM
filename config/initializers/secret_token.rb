8d41d3155ee5286cc14de6fd7f565e934191054a
Yauhen Kharuzhy <jekhor@gmail.com>
2012-01-21 13:08:17 +0300

    Initial commit
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MvoCRM::Application.config.secret_token = '3b9b487b29afd5665588c3d655cbd07803d3d423a3345cc582e3784a4d1a2b712dd03318aee834c627a4b938d88439d7db444bb7eda7efa8e348688ec11b9c18'
