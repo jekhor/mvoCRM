eeb96d18d9f943a5c575dc7384c7202131d7d0d0
Yauhen Kharuzhy <jekhor@gmail.com>
2012-03-06 21:10:41 +0300

    Design: logo, navbar
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+
+  it "should have an Sign In page at '/signin'" do
+    get '/signin'
+    response.should have_selector('title', :content => "About")
+  end
+
+  it "should have a Help page at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Help")
+  end
+
+end
