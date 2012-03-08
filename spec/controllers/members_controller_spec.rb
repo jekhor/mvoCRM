ee7606cd145386f6da25314b68a44238ce5f93e3
Yauhen Kharuzhy <jekhor@gmail.com>
2012-03-08 19:26:19 +0300

    Add tests for Members.show
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -6,6 +6,7 @@ describe MembersController do
 
   before :each do
     @base_title = "mvoCRM | "
+    @member = Factory(:member)
   end
 
   describe "GET 'index'" do
@@ -33,9 +34,30 @@ describe MembersController do
       response.should have_selector("title",
                                     :content => "#{@base_titile}New Member")
     end
-
   end
 
+  describe "GET 'show'" do
+    it "should be successful" do
+      get 'show', :id => @member
+      response.should be_success
+    end
+
+    it "should find the right member" do
+      get 'show', :id => @member
+      assigns(:member).should == @member
+    end
+
+    it "should have the right title" do
+      get 'show', :id => @member
+      response.should have_selector("title", :content => "#{@member.last_name} #{@member.given_names}")
+    end
+
+    it "should include user name in header" do
+      get 'show', :id => @member
+      response.should have_selector("h1", :content => "#{@member.last_name} #{@member.given_names}")
+    end
+
+  end
 
 
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+Factory.define :member do |m|
+  m.given_names     "Иван Петрович"
+  m.last_name       "Сидоров"
+  m.date_of_birth   "1984-07-21"
+  m.email           "test@example.com"
+end
