3df0cb09cfd8df7da031c7404f80ef054c39509e
Yauhen Kharuzhy <jekhor@gmail.com>
2012-05-22 17:09:32 +0300

    Add datepicker_su helper for xUSSR date format
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,11 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+
+  def datepicker_su(f, field, options = {})
+    date = f.object.send(field).nil? ? '' : f.object.send(field).strftime("%d.%m.%Y")
+    opts = {:value => date, :size => 10, :dateFormat => "dd.mm.yy", :selectOtherMonths => true}
+    opts.merge(options) if options.is_a?(Hash)
+    f.datepicker field, opts
+  end
 end
