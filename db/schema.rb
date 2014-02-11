c71e8bfc2771066ed1930062850dcdd1fd3290d3
Yauhen Kharuzhy <jekhor@gmail.com>
2014-02-11 16:05:22 +0300

    Remove settings from db schema (accidendally commited)
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -90,15 +90,4 @@ ActiveRecord::Schema.define(:version => 20140130131259) do
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
-  create_table "settings", :force => true do |t|
-    t.string   "var",                      :null => false
-    t.text     "value"
-    t.integer  "thing_id"
-    t.string   "thing_type", :limit => 30
-    t.datetime "created_at",               :null => false
-    t.datetime "updated_at",               :null => false
-  end
-
-  add_index "settings", ["thing_type", "thing_id", "var"], :name => "index_settings_on_thing_type_and_thing_id_and_var", :unique => true
-
 end
