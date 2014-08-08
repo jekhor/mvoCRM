4721acbd793b2bb5ca122a317fab0a3a0a61f7ec
Yauhen Kharuzhy <jekhor@gmail.com>
2014-08-08 09:39:53 +0300

    Donations: change date to datetime
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,4 @@
 class Donation < ActiveRecord::Base
-  attr_accessible :amount, :date, :document_number, :donor, :note, :payment_id
+  attr_accessible :amount, :datetime, :document_number, :donor, :note, :payment_id
   validates_uniqueness_of :payment_id
 end
